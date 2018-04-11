58406de2db0380ea580ce2c08f83a95c523bfd2b
Ritesh Singh <rkriteshsingh545@gmail.com>
2018-04-11 22:23:08 +0530

    Update MyServer.java

--- termwork5/MyServer.java
+++ termwork5/MyServer.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;  
 class MyServer{  
 public static void main(String args[])throws Exception{  
-ServerSocket ss=new ServerSocket(3000);  //establishing connection
+ServerSocket ss=new ServerSocket(3333);  //establishing connection
 Socket s=ss.accept();//return socket and establish connection between server and client  
 DataInputStream din=new DataInputStream(s.getInputStream()); //to get data from client 
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());  //to send data from server to client
